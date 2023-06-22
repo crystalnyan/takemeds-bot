515482b062d590f4bc39999f6dd2ab4d315080b5
Crystal Kate <crystalnyan@icloud.com>
2023-06-22 11:07:47 +0500

    register add, view, delete commands

--- bot.ts
+++ bot.ts
@@ -2,8 +2,6 @@ import { Bot } from "https://deno.land/x/grammy@v1.16.2/mod.ts";
 import { load } from "https://deno.land/std/dotenv/mod.ts";
 import {add_med, create_meds_table} from "./database/db.ts";
 
-"/database/db.ts"
-
 const env = await load();
 const token = env["BOT_TOKEN"];
 
@@ -13,6 +11,9 @@ create_meds_table();
 await bot.api.setMyCommands([
     { command: "start", description: "Start the bot" },
     { command: "help", description: "Show help text" },
+    {command: "add", description: "Add a new med"},
+    {command: "view", description: "View all my meds"},
+    {command: "delete", description: "Delete a med"}
 ]);
 
 
