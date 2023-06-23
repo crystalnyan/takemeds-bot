3aab1ac7100957ec1f051fc4db98ea138dd2c07a
Crystal Kate <crystalnyan@icloud.com>
2023-06-23 12:39:37 +0500

    map rows from db into Meds[]

--- types/meds.ts
+++ types/meds.ts
@@ -1,5 +1,18 @@
+import { get_meds_rows } from "../database/db.ts";
+
 export type Meds = {
     id: number;
     name: string;
     chat_id: number;
+}
+
+function get_meds(chat_id: number): Meds[] {
+    const rows = get_meds_rows(chat_id);
+
+    let meds: Meds[] = [];
+    for (const [id, name, chat_id] of rows) {
+        meds.push(<Meds>{id, name, chat_id});
+    }
+
+    return meds;
 }
