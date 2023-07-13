86c86a7b90f6a4b5dc97c794295209a55777de7a
Crystal Kate <crystalnyan@icloud.com>
2023-07-13 17:14:35 +0500

    update help text

--- delta/help.ts
+++ delta/help.ts
@@ -4,9 +4,19 @@ const composer = new Composer();
 
 export const message =
     "Use these commands to:" +
-    "\n/add - Add a new med" +
+    "\n\n/add - Add a new med" +
     "\n/view - View all my meds" +
-    "\n/delete - Delete a med";
+    "\n/delete - Delete a med" +
+    "\n\nHow to pass schedule information" +
+    "\nwhen you are adding a new med:" +
+    "\n/add <name> <hour> <minute> <day> <weekday>" +
+    "\n\nExample:" +
+    "\n/add Aspirin 12 30 * 1" +
+    "\n\nThis will schedule Aspirin to be taken" +
+    "\nat 12:30 every Monday." +
+    "\n\nNote:\n\"*\" means every. However, days may later" +
+    "\nbe restricted by specified weekdays" +
+    "\n\"*/3\" means each third minute/hour/day/month";
 
 composer.command("help", (ctx) => {
     ctx.reply(message);
