aba542d0d433b77b8ca837bcf37ecd8fb05389bd
Crystal Kate <crystalnyan@icloud.com>
2023-07-13 17:13:03 +0500

    [FIX] remove maxRuns limit of 1

--- cron.ts
+++ cron.ts
@@ -3,7 +3,6 @@ import {Context, Cron} from "./deps.ts";
 export function schedule(ctx: Context, name: string, cron: string) {
     new Cron(
         cron,
-        { maxRuns: 1},
         () => {
             ctx.reply(`You should take ${name} now!`);
         } );
