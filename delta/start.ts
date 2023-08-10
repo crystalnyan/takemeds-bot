ff56033da1a5eb31689b41efc03fccc235a8cf18
Crystal Kate <crystalnyan@icloud.com>
2023-08-10 18:17:59 +0500

    (start) create keyboard

--- delta/start.ts
+++ delta/start.ts
@@ -1,12 +1,20 @@
-import { Composer } from "../deps.ts";
+import { Composer, Keyboard } from "../deps.ts";
 
 const composer = new Composer();
 
 export const message =
     "Welcome, everynyan~ \nI'm a bot that reminds you to take your meds.";
 
+const keyboard = new Keyboard()
+    .text("Add a medication 💊").row()
+    .text("Veiw your meds").row()
+    .text("Help")
+    .resized();
+
 composer.command("start", (ctx) => {
-    ctx.reply(message);
+    ctx.reply(message, {
+        reply_markup: keyboard
+    });
 });
 
 export default composer;
