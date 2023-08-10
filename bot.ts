b8fbed8de8f4d8e486a89b5b0933d1a7451dc99e
Crystal Kate <crystalnyan@icloud.com>
2023-08-10 17:27:20 +0500

    add converation flavor and session

--- bot.ts
+++ bot.ts
@@ -1,12 +1,23 @@
-import { Bot, load, } from "./deps.ts";
+import {Bot, Context, load, session} from "./deps.ts";
 import { create_meds_table } from "./database/db.ts";
 import delta from "./delta/mod.ts";
 import {load_crons} from "./cron.ts";
+import {type Conversation,
+  type ConversationFlavor,
+  conversations,
+  createConversation} from "./deps.ts";
 
 const env = await load();
 const token = env["BOT_TOKEN"];
 
-export const bot = new Bot(token);
+type MyContext = Context & ConversationFlavor;
+type MyConversation = Conversation<MyContext>;
+
+export const bot = new Bot<MyContext>(token);
+
+bot.use(session({ initial: () => ({}) }));
+bot.use(conversations());
+
 await delta(bot);
 
 create_meds_table();
