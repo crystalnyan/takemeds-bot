ea2aab20a39981c9c6cea5c87d453ac59b52e9fe
Crystal Kate <e.muminova@uzinfocom.uz>
2023-08-21 18:15:14 +0500

    fix hour_up callback to update hour button text

--- delta/add.ts
+++ delta/add.ts
@@ -5,36 +5,16 @@ import { MyContext, MyConversation, bot } from "../bot.ts";
 
 const composer = new Composer();
 
-const time_labels = [
-    ["Hour 0-23", ""],
-    ["➖", "hour_down"],
-    ["0", ""],
-    ["➕", "hour_up"],
-    ["Minutes 0-59", ""],
-    ["➖", "minute_down"],
-    ["0", ""],
-    ["➕", "minute_up"]
-];
-
 async function add(conversation: MyConversation, ctx: MyContext) {
     await ctx.reply("Type the name for new medication:");
     const name = await conversation.form.text();
 
-    const time_keyboard = new InlineKeyboard();
-    for (const [label, data] in time_labels) {
-        if (label.includes("➕") || label.includes("Hour") || label.includes("Minutes")) {
-            time_keyboard.text(label, data).row();
-        } else {
-            time_keyboard.text(label, data);
-        }
-    }
-
-    /*const time_keyboard = new InlineKeyboard()
+    const time_keyboard = new InlineKeyboard()
     .text("Hour 0-23").row()
     .text("➖", "hour_down").text("0").text("➕", "hour_up").row()
     .text("Minutes 0-59").row()
     .text("➖", "minute_down").text("0").text("➕", "minute_down").row()
-    .text("Select", "select_time");*/
+    .text("Select", "select_time");
 
     await ctx.reply("Choose time for reminders:", {
         reply_markup: time_keyboard
@@ -60,10 +40,12 @@ export function add_med_convo() {
     // @ts-ignore working, taken from https://grammy.dev/plugins/conversations, still Deno complains
     bot.use(createConversation(add));
 
-    bot.hears("Add a medication 💊",  async (ctx) => {
+    bot.hears("Add a medication 💊", async (ctx) => {
         await ctx.conversation.enter("add");
     })
+}
 
+export function add_callbacks() {
     bot.callbackQuery("hour_up", async (ctx) => {
         const hour = ctx.callbackQuery.message?.reply_markup?.inline_keyboard[1][1].text;
 
@@ -71,26 +53,31 @@ export function add_med_convo() {
 
         const new_hour = (+hour + 1).toString();
 
-        if (ctx.callbackQuery.message == undefined ) return;
-        if (ctx.callbackQuery.message.reply_markup == undefined ) return;
+        if (ctx.callbackQuery.message == undefined) return;
+        if (ctx.callbackQuery.message.reply_markup == undefined) return;
 
-        ctx.callbackQuery.message.reply_markup.inline_keyboard[1][1].text = new_hour;
+        console.log(ctx.callbackQuery.message.reply_markup)
 
-        await ctx.editMessageReplyMarkup();
+         ctx.callbackQuery.message.reply_markup.inline_keyboard[1][1].text = new_hour;
+
+        await ctx.editMessageReplyMarkup({
+            reply_markup: ctx.callbackQuery.message.reply_markup
+        })
     })
 
     bot.callbackQuery("select_time", async (ctx) => {
-    const hour = ctx.callbackQuery.message?.reply_markup?.inline_keyboard[1][1].text;
-    const minutes = ctx.callbackQuery.message?.reply_markup?.inline_keyboard[3][1].text;
-    //await ctx.reply("Selected hour: " + hour + " minutes: " + minutes );
+        const hour = ctx.callbackQuery.message?.reply_markup?.inline_keyboard[1][1].text;
+        const minutes = ctx.callbackQuery.message?.reply_markup?.inline_keyboard[3][1].text;
+        //await ctx.reply("Selected hour: " + hour + " minutes: " + minutes );
 
-    const days = new InlineKeyboard()
-        .text("Weekdays", "weekdays").row()
-        .text("Month days", "month_days").row();
+        const days = new InlineKeyboard()
+            .text("Weekdays", "weekdays").row()
+            .text("Month days", "month_days").row();
 
-    await ctx.reply("Choose to remind on specific weekdays or days of a month?", {
-        reply_markup: days
-    })})
+        await ctx.reply("Choose to remind on specific weekdays or days of a month?", {
+            reply_markup: days
+        })
+    })
 }
 
 export default composer
--- delta/mod.ts
+++ delta/mod.ts
@@ -1,7 +1,7 @@
 import { Bot } from "../deps.ts";
 import start from "./start.ts";
 import help from "./help.ts";
-import add, {add_med_convo} from "./add.ts";
+import add, {add_callbacks, add_med_convo} from "./add.ts";
 import view from "./view.ts";
 import delete_med from "./delete.ts";
 
@@ -14,4 +14,5 @@ export default (bot: Bot)=> {
         .use(delete_med);
 
     add_med_convo();
+    add_callbacks();
 }
