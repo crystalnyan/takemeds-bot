3b62709512e29a10e53494443e9b4d1f299807c6
Crystal Kate <crystalnyan@icloud.com>
2023-08-24 15:56:12 +0500

    (view) add keyboard to operate on viewed meds

--- delta/view.ts
+++ delta/view.ts
@@ -1,6 +1,13 @@
 import { bot } from "../bot.ts";
+import { Keyboard } from "../deps.ts";
 import {view_meds} from "../types/med.ts";
 import {keyboard} from "./start.ts";
+
+const meds_kb = new Keyboard()
+    .text("Delete a med").row()
+    .text("Continue")
+    .oneTime()
+    .resized();
 
 export function view_callbacks() {
     bot.hears("View your meds", async (ctx) => {
@@ -8,9 +15,18 @@ export function view_callbacks() {
 
         const meds = view_meds(ctx.chat.id);
 
-        if (meds.length == 0) return ctx.reply("You don't have any meds added!");
-        return await ctx.reply(meds, {
+        if (meds.length == 0) return ctx.reply("You don't have any meds added!", {
             reply_markup: keyboard
         });
+
+        return await ctx.reply(meds, {
+            reply_markup: meds_kb
+        });
+    })
+
+    bot.hears("Continue", async (ctx) =>{
+        await ctx.reply("Anything else?", {
+            reply_markup: keyboard
+        })
     })
 }
